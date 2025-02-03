7c6ecb7022b133e59b66efcd8e36cf799d2b477b
Lore Goethals <lore.goethals2@etu.univ-lorraine.fr>
2025-02-03 16:01:25 +0100

    Committing before pulling branch

--- src/routes/pizzasEndpoint.ts
+++ src/routes/pizzasEndpoint.ts
@@ -1,10 +1,16 @@
 import { Hono } from "hono";
-import { pizzasCollection } from "../data";
+//import { pizzasCollection } from "../data";
 import { HTTPException } from "hono/http-exception";
+import { Database } from "bun:sqlite";
+import type { Pizza } from "../types";
+import { connectDatabase } from "../db/database";
 
 const pizzasEndpoint = new Hono();
+const dbPath = "./pizzas-napoli.db";
 
 pizzasEndpoint.get("/:id", (c) => {
+  //récupère l'id dans le path de l'URI
+  //on convertit l'id en nombre
   const id: number = parseInt(c.req.param("id"));
 
   //si la conversion de l'id ne permet pas d'obtenir un nombre (undefined, null ou NaN)
@@ -13,30 +19,39 @@ pizzasEndpoint.get("/:id", (c) => {
     throw new HTTPException(400);
   }
 
-  //récupère l'id dans le path de l'URI
-  //on convertit l'id en nombre
-  const pizza = pizzasCollection.find((p) => p.id === id);
   //on cherche la pizza dont l'id est égal au paramètre id récupéré
 
-  return c.json({ pizza });
-});
+  try {
+    const db = connectDatabase();
 
-pizzasEndpoint.get("/", (c) => {
-  const { base, price } = c.req.query(); //création dynamique d'une variable base, par destructuration de l'objet retourné par c.req.query()
+    const query = await db.query("SELECT * FROM pizzas").as(Pizza);
+    //const pizzas = query.all();
+    const pizza_demande = query.find((p:Pizza) => p.id === id);
 
-  let pizzas = pizzasCollection;
+    return pizza_demande;
 
-  if (base) {
-    pizzas = pizzas.filter((p) => p.base === base);
+  } catch (error) {
+    console.error(error);
   }
 
-
-  if (price) {
-    const priceNumber = Number(price);
-    pizzas = pizzas.filter((p) => p.price === priceNumber);
-  }
-
-  return c.json({ pizzas });
 });
+
+// pizzasEndpoint.get("/", (c) => {
+//   const { base, price } = c.req.query(); //création dynamique d'une variable base, par destructuration de l'objet retourné par c.req.query()
+
+//   let pizzas = pizzasCollection;
+
+//   if (base) {
+//     pizzas = pizzas.filter((p) => p.base === base);
+//   }
+
+
+//   if (price) {
+//     const priceNumber = Number(price);
+//     pizzas = pizzas.filter((p) => p.price === priceNumber);
+//   }
+
+//   return c.json({ pizzas });
+// });
 
 export default pizzasEndpoint;
